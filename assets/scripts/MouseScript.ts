854436c8c63bdf47d352dad72cabaddb88a30077
Lisa Vogt <lisavogt.sf@gmail.com>
2019-03-25 22:34:13 -0700

    adds first clickable outfit to decision scene, other assets loaded

---
+++ assets/scripts/MouseScript.ts
@@ -0,0 +1,19 @@
+const {ccclass, property} = cc._decorator;
+@ccclass
+export default class MouseScript extends cc.Component {
+    onLoad () {
+        this.node.on(cc.Node.EventType.MOUSE_DOWN,(e:cc.Event.EventMouse)=>{
+            console.log("Mouse down on top of selected node");
+            e.bubbles = false; // Handled event don't let it propogate!
+            cc.director.loadScene("outcome-scene")
+        });
+        this.node.on(cc.Node.EventType.MOUSE_LEAVE,(e:cc.Event.EventMouse)=>{
+            console.log("Mouse no longer over " + e.currentTarget.name);
+        });
+        this.node.parent.on(cc.Node.EventType.MOUSE_DOWN,(e:cc.Event.EventMouse)=>{
+            // add some visual feedback here, that it was pressed before scene change?
+            // wonder how that timing would even work
+            console.log("Mouse button pressed on parent.  Button: " + e.getButton());
+        });
+    }
+}
